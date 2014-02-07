60a5135722bd1fd21ff28a80617106c571d7472a
conorfi <conor.fitzgerald@artsalliancemedia.com>
2014-02-07 12:55:17 +0000

    Adding changes to the env files - added under gatekeeper_develop branch all further changes will be made under a screen_writer develop branch

--- env_config/test_env.py
+++ env_config/test_env.py
@@ -8,22 +8,41 @@ config = get_common_config()
 
 gatekeeper_config = get_default_service_config()
 gatekeeper_config.update({
+    'scheme': 'https',
     'host': 'app-t01',
     'ip': '10.20.254.142',
     'port': '8070',
     'db_host': '10.20.254.142',
     'db_name': 'gatekeeper',
-    'dummyapp_host': 'app-t01'
+    'dummyapp_host': 'app-t01',
+    'db_type' : 'postgresql',
+    'db_credentials' : 'postgres:postgres'
 })
 
 set_gatekeeper_config(config, **gatekeeper_config)
 
 courier_config = get_default_service_config()
 courier_config.update({
+    'scheme': 'https',
     'host': 'app-t01',
     'ip': '10.20.254.142',
     'port': '10001',
     'db_host': '10.20.254.142',
-    'db_name': 'courier'
+    'db_name': 'courier',
+    'db_type' : 'postgresql',
+    'db_credentials' : 'postgres:postgres'
 })
 set_courier_config(config, **courier_config)
+
+
+screenwriter_config = get_default_service_config()
+screenwriter_config.update({
+    'scheme': 'http',
+    'host': '10.0.2.15',
+    'ip': '10.0.2.15',
+    'port': '8080',
+    'db_host': '10.0.2.15',
+    'db_name' : '/aam-lms/db/cinema_services.db',
+    'db_type' : 'sqlite:///'
+})
+set_screenwriter_config(config, **screenwriter_config)
