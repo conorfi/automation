1b417d454e5cea266d1f6f3920e4921b75e02a7a
vthimmaiah <vaishnavi.thimmaiah@artsalliancemedia.com>
2014-04-04 12:40:34 +0100

    Adding test_sample.py

---
+++ test_cases/test_sample.py
@@ -0,0 +1,17 @@
+import requests
+from testconfig import config
+from nose.plugins.attrib import attr
+
+class TestSample:
+    '''A class for API testing the OAuth web service'''
+
+@attr(env=['test'], priority=1)
+def test_get_requests():
+    server = config['google-server']['host']
+    #server = 'http://localhost'
+    r = requests.get(server)
+    text = r.text
+    assert r.status_code == requests.codes.ok
+
+    print r.text
+    print r.status_code
